c154b506e139b2473b3966532483f1b53e542679
miro smelko <miro.smelko@windowslive.com>
2017-10-01 11:08:07 +0200

    code cleanup

--- GradientDescentAlgorithmsComparison/Main.py
+++ GradientDescentAlgorithmsComparison/Main.py
@@ -1,11 +1,20 @@
+import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+
+from GradientDescentAlgorithmsComparison.HyperParameters import HyperParameters
 from GradientDescentAlgorithmsComparison.NeuralNetwork import NeuralNetwork
 
 # Read input data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=False)
 
+# Create parameters for neural network
+hp = HyperParameters()
+hp.num_steps = 1000  # less parameters than default = who would want to wait that long :)
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=hp.learning_rate)
+
 # Create neural network
-nn = NeuralNetwork()
+nn = NeuralNetwork(optimizer, hp)
 
 # Train neural network
 nn.train(mnist.train.images, mnist.train.labels)
--- GradientDescentAlgorithmsComparison/NeuralNetwork.py
+++ GradientDescentAlgorithmsComparison/NeuralNetwork.py
@@ -3,53 +3,62 @@ Trains neural network using tensorflow
 """
 
 import tensorflow as tf
-from GradientDescentAlgorithmsComparison.HyperParameters import HyperParameters
 
 
 class NeuralNetwork:
 
-    hp = HyperParameters()
+    hp = None
     model = None
+    optimizer = None
 
-    def __init__(self):
+    def __init__(self, optimizer, hyper_parameters):
         self.model = tf.estimator.Estimator(self.model_fn)
+        self.optimizer = optimizer
+        self.hp = hyper_parameters
 
+    """
+    Builds neural network
+    """
     def model_fn(self, features, labels, mode):
-        # Build the neural network
-
-        # TF Estimator input is a dict, in case of multiple inputs
+        # we just need the images from the dictionary
         x = features['images']
-        # Hidden fully connected layer with 256 neurons
+
+        # Hidden fully connected layer
         layer_1 = tf.layers.dense(x, self.hp.n_hidden_1)
-        # Hidden fully connected layer with 256 neurons
+
+        # Hidden fully connected layer
         layer_2 = tf.layers.dense(layer_1, self.hp.n_hidden_2)
+
         # Output fully connected layer with a neuron for each class
         logits = tf.layers.dense(layer_2, self.hp.num_classes)
 
         # Predictions
-        pred_classes = tf.argmax(logits, axis=1)
+        y_ = tf.argmax(logits, axis=1)
 
         # Define loss and optimizer
         loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
             logits=logits, labels=tf.cast(labels, dtype=tf.int32)))
 
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.hp.learning_rate)
-        train_op = optimizer.minimize(loss_op, global_step=tf.train.get_global_step())
+        # Create train operation using given optimizer
+        train_op = self.optimizer.minimize(loss_op, global_step=tf.train.get_global_step())
 
         # Evaluate the accuracy of the model
-        acc_op = tf.metrics.accuracy(labels=labels, predictions=pred_classes)
+        acc_op = tf.metrics.accuracy(labels=labels, predictions=y_)
 
         # TF Estimators requires to return a EstimatorSpec, that specify
         # the different ops for training, evaluating, ...
         estim_specs = tf.estimator.EstimatorSpec(
             mode=mode,
-            predictions=pred_classes,
+            predictions=y_,
             loss=loss_op,
             train_op=train_op,
             eval_metric_ops={'accuracy': acc_op})
 
         return estim_specs
 
+    """
+    Trains neural network
+    """
     def train(self, x, y):
         # Define the input function for training
         input_fn = tf.estimator.inputs.numpy_input_fn(
@@ -59,6 +68,9 @@ class NeuralNetwork:
         # Train the Model
         self.model.train(input_fn, steps=self.hp.num_steps)
 
+    """
+    Evaluates the trained model against training set
+    """
     def evaluate(self, x, y):
         # Define the input function for evaluating
         input_fn = tf.estimator.inputs.numpy_input_fn(
